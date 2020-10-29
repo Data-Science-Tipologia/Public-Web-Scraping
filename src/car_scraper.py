02f39b0a5eb90b8a59eee1fb3ec6726b0ca3cd1f
Jaime <jaimegf98@uoc.edu>
2020-10-29 18:36:41 +0100

    Get global and brand links

--- src/car_scraper.py
+++ src/car_scraper.py
@@ -60,6 +60,7 @@ class Car:
             'year': self.year
         }
 
+
 class CarsScraper:
 
     def __init__(self, url):
@@ -79,7 +80,6 @@ class CarsScraper:
 
         browser = webdriver.Chrome(options=opts)
 
-
         try:
             browser.get(url)
             time.sleep(3)
@@ -92,9 +92,54 @@ class CarsScraper:
 
             cookies_button.click()
 
-        except :
+        except:
             print("Error in link : {}".format(url))
             return browser
 
         return browser
+
+    def get_links(self, browser):
+        """
+        Return a list of links_class from browser driver
+        """
+        links = []
+        try:
+            element = WebDriverWait(browser, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "central-menu"))
+            )
+            link_KM0 = element.find_element_by_link_text(self.km0)
+            link_Sec = element.find_element_by_xpath("//*[@id='header']/div["
+                                                     "2]/ul/li[4]/ul/li[1]/a")
+            links.append(link_KM0.get_attribute('href'))
+            links.append(link_Sec.get_attribute('href'))
+        except:
+            print("Error in get_links function")
+        return links
+
+    def brands_links(self, link, car_class):
+        """Return a list with every link for each brand"""
+        links_brands = []
+        cl_name = ""
+        if car_class == self.news:
+            cl_name = "cc_grid_makes"
+        elif car_class == self.km0 or car_class == self.second:
+            cl_name = "row.cc_makes "
+        browser = self.din_scraper(link)
+        try:
+            element = WebDriverWait(browser, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, cl_name)
+                                               )
+            )
+            a_sect = element.find_elements_by_tag_name('a')
+            for a in a_sect:
+                try:
+                    if validators.url(a.get_attribute('href')):
+                        links_brands.append(a.get_attribute('href'))
+                except:
+                    pass
+        finally:
+            pass
+        browser.quit()
+        return links_brands
+
 
